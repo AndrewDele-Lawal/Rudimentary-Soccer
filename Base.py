0f7746e46aec1976f90f1c8ac53cc76df8022c3c
AndrewDele-Lawal <andrewdelelawal2023@gmail.com>
2025-08-15 19:05:56 -0400

    added input correction

--- Base.py
+++ Base.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 # Super Limited Soccer
 
@@ -26,11 +27,27 @@ while (Player.gettally() < pointsGoal) :
   # User input for which direction they want to kick the ball
   kick = str(input("Do you wish to kick the ball: 'Straight', 'Left', or to the 'Right'\n"))
 
+  # Handles invalid inputs
+  while kick != "Straight" and kick != "Left" and kick != "Right":
+    print("Input the right thing dummy")
+    time.sleep(2)
+    kick = str(input("Alright lets do this again lil bro.\nEnter 'Straight', 'Left', or 'Right' Otherwise your ##### won't be able to play the game:\n"))
+    time.sleep(2)
+
   # Generates a new direction every time the code loops
   Directional_Key = random.SystemRandom().randint(0, len(Direction) - 1)
 
+  print("Goalie is gearing up to block")
+  time.sleep(2)
+
+  # Score logic
   if kick == Direction[Directional_Key]:
-    print("The Goally jumped", Direction[Directional_Key], "blocking the ball")
+    print("The Goalie jumped", Direction[Directional_Key], "blocking the ball")
   else:
-    print("The Goally jumped", Direction[Directional_Key], "allowing your shot to hit ", kick)
+    print("The Goalie jumped", Direction[Directional_Key], "allowing your shot to hit ", kick)
     Player.addTally()
+    time.sleep(1)
+    print("Current score is",Player.gettally(),"\n")
+  print("GAME OVER, you got", pointsGoal)
+
+
